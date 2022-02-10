77ca2497c65ac24f31f472cdea134cf0818f1651
Michael Cortez-Mejia <you@example.com>
2022-02-09 17:53:25 -0800

    added redirect for error handling

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,21 +1,47 @@
 var issueContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
+var repoNameEl = document.querySelector("#repo-name");
+
+var getRepoName = function () {
+    // grab repo name from url query string
+    var queryString = document.location.search;
+    var repoName = queryString.split("=")[1];
+    
+    // check if query parameter is available
+    if (repoName){
+        // display repo name on the page
+        repoNameEl.textContent = repoName;
+
+        getRepoIssues(repoName);
+    } else {
+        // if no repo was given, redirect to the homepage
+        document.location.replace("./index.html");
+    }
+};
 
 
 var getRepoIssues = function (repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
 
+    // make a get request to url
     fetch(apiUrl)
         .then(function (response) {
-            // if request was successful 
+            // request was successful 
             if (response.ok) {
                 response.json()
                     .then(function (data) {
                         // pass response data to dom function
                         displayIssues(data);
+
+                        // check if api has paginated issues 
+                        if (response.headers.get("Link")){
+                            displayWarning(repo);
+                        }
                     });
             }
             else {
-                alert("There was a problem with your request!")
+                // if not successful, redirect to homepage
+                document.location.replace("./index.html");
             }
         });
 };
@@ -59,4 +85,20 @@ var displayIssues = function(issues) {
     }
 };
 
-getRepoIssues("mcortezm1/git-it-done");
+// if there are more than 30 repo issues 
+var displayWarning = function(repo) {
+    // add text to warning container 
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    // link element to open page 
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank"); // open link page in new tab
+
+    // append to warning container 
+    limitWarningEl.appendChild(linkEl);
+};
+
+getRepoName();
+
